691e5858e572308762b2931f75c1bfa18511886e
shotpoet86 <curlybracemaster@gmail.com>
2023-09-05 12:56:54 -0400

    - updated onInput function to loop through results and list div elements on screen with movie title and poster image

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ const fetchData = async (searchTerm) => {
             s: searchTerm,
         }
     })
-    console.log(searchTerm)
-    /*    for (let i of response.data.Search) {
-            console.log(`Movie is ${i.Title} released in ${i.Year} `)
-        }*/
+    if (response.data.Error) {
+        return []
+    }
+    return response.data.Search
 }
 
 /*connect to input field of html file*/
 const input = document.querySelector('input');
 
 
-const onInput = event => fetchData(event.target.value)
+const onInput = async (event) => {
+    const movies = await fetchData(event.target.value)
+    for (let movie of movies) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <img src="${movie.Poster}" alt="movie posters"/>
+        <h1>${movie.Title}</h1>`
+        document.querySelector('.target').appendChild(div)
+    }
+}
 
 /*event listen for input change and perform data request from omdb per user input*/
 input.addEventListener('input', debounce(onInput, 500))
