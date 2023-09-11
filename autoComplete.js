d8474e584605d16394b0ce3a5964bbc47b24ede9
shotpoet86 <curlybracemaster@gmail.com>
2023-09-11 12:56:01 -0400

    - added comparison functionality - functionality to determine which movie did better based on attributes displayed on screen and reflect color to show loss. - refactored various code to enhance modularity and reuse of autocomplete object -Project considered complete

--- autoComplete.js
+++ autoComplete.js
@@ -1,4 +1,8 @@
+/*autocomplete template that takes 5 parameters and displays information from arguments passed by dev
+* to end user*/
 const createAutoComplete = ({root, renderOption, onOptionSelect, inputValue, fetchData}) => {
+
+    /*model of dropdown to be displayed in root element*/
     root.innerHTML = `
         <label>
             <b>Search</b>
@@ -14,7 +18,6 @@ const createAutoComplete = ({root, renderOption, onOptionSelect, inputValue, fet
     const input = root.querySelector('input');
     const dropdown = root.querySelector('.dropdown')
     const resultsWrapper = root.querySelector('.results')
-
     /*Uses for/of loop to display content*/
     const onInput = async (event) => {
         const items = await fetchData(event.target.value)
@@ -30,17 +33,12 @@ const createAutoComplete = ({root, renderOption, onOptionSelect, inputValue, fet
 
         /*loop through item results*/
         for (let item of items) {
-
             /*create an anchor element for user to select gaining more info about item*/
             const option = document.createElement('a');
-
-
             /*append 'dropdown-item' class to current option anchor element during loop*/
             option.classList.add('dropdown-item')
-
             /*add inner-html to option anchor element with results of each item title*/
             option.innerHTML = renderOption(item)
-
             /*listen for user click on any item in dropdown menu*/
             option.addEventListener('click', () => {
                 /*close dropdown menu*/
@@ -51,23 +49,19 @@ const createAutoComplete = ({root, renderOption, onOptionSelect, inputValue, fet
                 * detailed results for specific item*/
                 onOptionSelect(item)
             })
-
-            /*append option anchor to results class in html content*/
             resultsWrapper.appendChild(option)
         }
     }
 
     /*event listen for input change and perform data request from api per user input. Calls
- * debounce function from utils.js with 2nd argument to determine wait time between user input and api call*/
+ * debounce function from utils.js with 2nd argument to determine wait time between user input and api call.
+ * Delay can be changed by dev using 2nd argument*/
     input.addEventListener('input', debounce(onInput, 1000))
-
     /*global event listener that handles closing the dropdown menu if focus is lost*/
     document.addEventListener('click', event => {
         if (!root.contains(event.target)) {
             dropdown.classList.remove('is-active')
         }
-        console.log(event.target)
     })
-
 }
 
--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-/*request detailed data from api to show when user selects specific movie from dropdown menu*/
-const fetchDetailedData = async (movie) => {
-    /*store data request in property of response*/
-    const detailedResponse = await axios.get('http://www.omdbapi.com/', {
-        /*add axios query parameters for request*/
-        params: {
-            apikey: "32c05230",
-            i: movie.imdbID,
-        }
-    })
-    /*return empty array if response results in error*/
-    if (detailedResponse.data.Error) {
-        return []
-    }
-    /*return data if no error*/
-    return detailedResponse.data
-}
-
-createAutoComplete({
-    root: document.querySelector('.autocomplete'),
+/*autocompleteconfig is template with required createAutoComplete function arguments. Dev will need to provide
+* api to fetch for 'root' property of object*/
+const autoCompleteConfig = {
     renderOption(movie) {
         /*Ensures no image will show if unavailable or broken = "N/A"*/
         const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
         return `
         <img src="${imgSrc}" alt="movie posters"/>
         ${movie.Title} (${movie.Year})`
-    },
-    onOptionSelect(movie) {
-        return onMovieSelect(movie)
     },
     inputValue(movie) {
         return movie.Title
@@ -48,19 +28,87 @@ createAutoComplete({
         /*return data if no error*/
         return response.data.Search
     }
-})
-
-/*when user selects specific movie from dropdown menu*/
-const onMovieSelect = async (movie) => {
-    /*store api call in movieDetails passing in user selected movie using imdbID to call more
-    * information about specific movie*/
-    const movieDetails = await fetchDetailedData(movie)
-    /*add innerHTML to summary class in html file*/
-    document.querySelector('.summary').innerHTML = movieTemplate(movieDetails);
 }
+/*call createAutoComplete object by spreading imdb config and providing root property information*/
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#right-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('.right-summary'), 'right')
+    },
+})
+/*call createAutoComplete object by spreading imdb config and providing root property information*/
+createAutoComplete({
+    ...autoCompleteConfig,
+    root: document.querySelector('#left-autocomplete'),
+    onOptionSelect(movie) {
+        document.querySelector('.tutorial').classList.add('is-hidden')
+        onMovieSelect(movie, document.querySelector('.left-summary'), 'left')
+    },
+})
+/*delcare variables to use in onMovieSelect function*/
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        /*axios query parameters for request*/
+        params: {
+            apikey: "32c05230",
+            i: movie.imdbID,
+        }
+    })
+    if (response.data.Error) {
+        return []
+    }
+    /*add imdb data to summary class in html file*/
+    summaryElement.innerHTML = movieTemplate(response.data);
+
+    /*determine which movie receives data from imdb api*/
+    if (side === 'left') {
+        leftMovie = response.data;
+    } else {
+        rightMovie = response.data;
+    }
+    if (leftMovie && rightMovie) {
+        runComparison();
+    }
+}
+
+const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('.left-summary .notification')
+    const rightSideStats = document.querySelectorAll('.right-summary .notification')
+    /*changes losing movie attribute color to yellow reflecting loss by evaluating data class appended to each
+    * article in movieTemplate function below*/
+    leftSideStats.forEach((leftStat, idx) => {
+        const rightStat = rightSideStats[idx]
+        const leftSideValue = parseInt(leftStat.dataset.value)
+        const rightSideValue = parseInt(rightStat.dataset.value)
+        if (rightSideValue > leftSideValue) {
+            leftStat.classList.remove('is-primary')
+            leftStat.classList.add('is-warning')
+        } else {
+            rightStat.classList.remove('is-primary')
+            rightStat.classList.add('is-warning')
+        }
+    })
+}
+
 
 /*displays detailed information about user selected movie*/
 const movieTemplate = (movieDetail) => {
+    /*converts important comparison information to numbers for evaluation*/
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''))
+    const metaScore = parseInt(movieDetail.Metascore)
+    const imdbRating = parseFloat(movieDetail.imdbRating)
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ''))
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word) => {
+        const value = parseInt(word)
+        if (isNaN(value)) {
+            return prev
+        } else return prev + value
+    }, 0)
+    /*return html template*/
     return `
     <article class="media">
         <figure class="media-left">
@@ -76,23 +124,23 @@ const movieTemplate = (movieDetail) => {
             </div>
         </div>
     </article> 
-<article class="notification is-primary">
+<article data-value="${awards}" class="notification is-primary">
 <p class="title">${movieDetail.Awards}</p>
 <p class="subtitle">Awards</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${dollars}" class="notification is-primary">
 <p class="title">${movieDetail.BoxOffice}</p>
 <p class="subtitle">Box Office</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${metaScore}" class="notification is-primary">
 <p class="title">${movieDetail.Metascore}</p>
 <p class="subtitle">Metascore</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${imdbRating}" class="notification is-primary">
 <p class="title">${movieDetail.imdbRating}</p>
 <p class="subtitle">IMDB Rating</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${imdbVotes}" class="notification is-primary">
 <p class="title">${movieDetail.imdbVotes}</p>
 <p class="subtitle">IMDB Votes</p>
 </article>
